0677b6bc21ba2ebbcdcdf7ae4a3f7ac1ceec831c
3DS <3ds@bglab.local>
2024-04-04 22:02:34 -0300

    trafico

--- Aplicativo contador/atm/lib/views/contato.dart
+++ Aplicativo contador/atm/lib/views/contato.dart
@@ -15,7 +15,14 @@ class _ContateState extends State<Contate> {
       
       body: Column(children: [Row(children: [Image.asset("Imagens/detalhe_contato.png"), 
       Text("Entre em contato")],),
-            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae risus consequat, pretium velit quis, tristique purus. Morbi lacinia, eros sit amet tincidunt aliquam, felis nibh ultrices nisl, nec pellentesque augue turpis luctus risus. Ut hendrerit, erat ut ullamcorper dapibus, mi massa mattis ante, vitae consectetur orci purus nec nulla. Vivamus placerat diam quis rhoncus ultricies. Etiam semper lacus vel ultrices laoreet. Nullam sed fringilla massa, blandit consequat enim. Sed venenatis purus at ligula convallis elementum. Sed faucibus, sapien vel porttitor tempus, quam nunc iaculis diam, id maximus diam sem quis leo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque eu magna in ex feugiat laoreet.")
+
+       Padding(padding: EdgeInsets.only(top: 16),
+            child: Text("Email@email.com"),
+            ),
+
+            Padding(padding: EdgeInsets.only(top: 16),
+            child: Text("11978456123"),
+            ),
       ],),
     
     );
--- Aplicativo contador/atm/lib/views/servico.dart
+++ Aplicativo contador/atm/lib/views/servico.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class Servico extends StatefulWidget {
@@ -15,7 +17,11 @@ class _ServicoState extends State<Servico> {
        
       body: Column(children: [Row(children: [Image.asset("Imagens/detalhe_servico.png"), 
       Text("Nossos Serviços")],),
-            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae risus consequat, pretium velit quis, tristique purus. Morbi lacinia, eros sit amet tincidunt aliquam, felis nibh ultrices nisl, nec pellentesque augue turpis luctus risus. Ut hendrerit, erat ut ullamcorper dapibus, mi massa mattis ante, vitae consectetur orci purus nec nulla. Vivamus placerat diam quis rhoncus ultricies. Etiam semper lacus vel ultrices laoreet. Nullam sed fringilla massa, blandit consequat enim. Sed venenatis purus at ligula convallis elementum. Sed faucibus, sapien vel porttitor tempus, quam nunc iaculis diam, id maximus diam sem quis leo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque eu magna in ex feugiat laoreet.")
+   
+      const Padding(padding: EdgeInsets.only(left: 10),
+            child: Text("Nossos serviços: Agiotagem/Trafico de influencia/Crimes politicos/Trafico de Eight"),
+            ),
+
       ],),
     );
   }
